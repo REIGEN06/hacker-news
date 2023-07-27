683838ceb116740287cc68ba464bb11753d38b81
Daniil Yakubov <daniil-yak@yandex.ru>
2023-07-27 15:08:49 +0300

    feat: добавлен поиск по новостям

--- src/components/StoriesList.tsx
+++ src/components/StoriesList.tsx
@@ -1,16 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
 import { useQuery } from 'react-query';
 import { RootState } from '@redux/store';
 import { setStories } from '@actions/actionCreator';
 import { ArrayStoryType, StoryType } from '@const/storyConst';
 import { StyledButton } from '@ui/Buttons';
 import { Title } from '@ui/Text';
+import { Row } from '@ui/Sections';
 import { getStories } from '@api/hnApi';
 import StoryCard from '@components/StoryCard';
 
 const StoriesList = () => {
 	const dispatch = useDispatch();
+	const [input, setInput] = useState('');
+
 	const { isLoading, isError, isSuccess, data, refetch, isFetching } = useQuery(
 		'stories',
 		() => getStories(),
@@ -23,6 +27,10 @@ const StoriesList = () => {
 		(state: RootState): ArrayStoryType => state.news.stories
 	);
 
+	const filteredNews = news?.filter((story) => {
+		return story.title?.toLowerCase().includes(input.toLowerCase());
+	});
+
 	useEffect(() => {
 		if (isSuccess) {
 			dispatch(setStories(data));
@@ -31,13 +39,27 @@ const StoriesList = () => {
 
 	return (
 		<>
-			<StyledButton onClick={() => refetch()}>
-				{isFetching ? <p>Обновляется...</p> : <p>Обновить</p>}
-			</StyledButton>
+			<Row>
+				<StyledInput
+					type="text"
+					name="name"
+					placeholder="Введите название поста"
+					onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+						setInput(event.target.value)
+					}
+				/>
+				<StyledButton $padding="0px 5px" onClick={() => refetch()}>
+					{isFetching ? (
+						<p>Список обновляется...</p>
+					) : (
+						<p>Обновить список новостей</p>
+					)}
+				</StyledButton>
+			</Row>
 
 			{isError && <Title>ОШИБКА</Title>}
 			{isLoading && <Title>Посты загружаются...</Title>}
-			{news?.map((story: StoryType) => {
+			{filteredNews?.map((story: StoryType) => {
 				return story ? <StoryCard key={story.id} data={story} /> : null;
 			})}
 		</>
@@ -45,3 +67,12 @@ const StoriesList = () => {
 };
 
 export default StoriesList;
+
+const StyledInput = styled.input`
+	background: ${(props) => props.theme.BGcolors.gray};
+	border-radius: 50px;
+	padding: 10px;
+	display: flex;
+	align-items: center;
+	gap: 5px;
+`;
